c8c9693f43b1fd5ffe8129561e1829872db8a009
Dan Reardon <dreardon1021@gmail.com>
2019-12-17 17:18:34 -0700

    Add button funcitonallity to highlight selected hat item

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 var outfits = require('outfit.js');
+
+hatButtons.addEventListener('click', highlightTarget)
+
+var hatButtons = document.querySelector('.col1-buttons')
+var hatNodeList = document.querySelectorAll('.hat-button')
+
+
+function highlightTarget(event) {
+  console.log('hi')
+  for (i = 0; i < hatNodeList.length; i++) {
+    hatNodeList[i].classList.remove('selected')
+  };
+  if(event.target.classList.contains('hat-button')) {
+    event.target.classList.add('selected');
+  };
+};
